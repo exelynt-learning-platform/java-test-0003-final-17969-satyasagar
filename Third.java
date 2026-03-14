705f5cbc09b1ae4f9312631cb7bb4a9073787b1f
Sagar <sp120727@gmail.com>
2026-03-14 16:17:58 +0530

    sol3upd

--- Third.java
+++ Third.java
@@ -1,18 +1,18 @@
-public class Second {
+public class Third {
     public static void main(String[] args) {
-        int n = 6; // number of rows
+        int n = 6; 
 
         for (int i = 1; i <= n; i++) {
             int num;
 
             if (i % 2 == 1)
-                num = 1;   // odd row starts with 1
+                num = 1; 
             else
-                num = 0;   // even row starts with 0
+                num = 0;   
 
             for (int j = 1; j <= i; j++) {
                 System.out.print(num + " ");
-                num = 1 - num; // toggle between 0 and 1
+                num = 1 - num; 
             }
 
             System.out.println();
